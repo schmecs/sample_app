0b7df3d169ceb5a47e973643097ad91aed4f83c4
Rebecca <rblum@patientslikeme.com>
2017-11-10 16:44:31 -0500

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
 
+	def base_title
+		"Ruby on Rails Tutorial Sample App"
+	end
+
 	#Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-		base_title = "Ruby on Rails Tutorial Sample App"
 		if page_title.empty?
-			base_title
+			self.base_title
 		else
-			page_title + " | " + base_title
+			page_title + " | " + self.base_title
 		end
 	end
 end
