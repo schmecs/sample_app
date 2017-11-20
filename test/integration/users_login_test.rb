a40ea8f5dbb82d7a45a321e54cb42891015df7ef
Rebecca <rblum@patientslikeme.com>
2017-11-20 18:13:04 -0500

    post-login integration test updates

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-	test "user logs in successfully" do
+	def setup
+		@user = users(:rebecca)
+	end
+
+	test "failed login should reload login and show flash" do
 		get login_path
 		assert_template 'sessions/new'
 		post login_path, params: { session: { email: "not@user", 
@@ -11,6 +15,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_not flash.empty?
 		get root_path
 		assert flash.empty?
+	end
+
+	test "successful login should update params and redirect to user profile" do
+		get login_path
+		post login_path, params: { session: { email: @user.email,
+			password: 'password' } }
+		assert_redirected_to @user
+		follow_redirect!
+		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", logout_path
+		assert_select "a[href=?]", user_path(@user)
 	end
 
 end
