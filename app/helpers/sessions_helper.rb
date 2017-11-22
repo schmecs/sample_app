ee78285218b6ec4d4f950c60787a905606b15d86
Rebecca <rblum@patientslikeme.com>
2017-11-22 15:51:25 -0500

    implement logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,8 +10,15 @@ module SessionsHelper
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
+	# Returns whether session is logged-in.
 	def logged_in?
 		!current_user.nil?
 	end
+
+	# logs out current user.
+	def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		end
 		follow_redirect!
 		assert_template 'users/show'
+		assert is_logged_in?
 		assert_not flash.empty?
 		assert_select 'div.alert-success', count: 1
 	end
