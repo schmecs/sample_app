266fb5e133c79f9f58a76a3faafbaee75e55f2dd
Rebecca <rblum@patientslikeme.com>
2017-12-01 07:21:44 -0500

    Implement user profile update, login authorization, and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -25,7 +28,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -36,4 +40,11 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email, :password,
   																	:password_confirmation)
   	end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 	end
 
 	test "unsuccessful edit" do
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		patch user_path(@user), params: { user: { name: "",
@@ -17,6 +18,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		assert_select 'form[class="edit_user"]'
 		assert_select 'div#error_explanation'
 		assert_select 'div.alert', /4/
+	end
+
+	test "successful edit" do
+		log_in_as(@user)
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		name = "Foo Bar"
+		email = "foo@bar.com"
+		patch user_path(@user), params: { user: { name: name,
+																							email: email,
+																							password: "",
+																							password_confirmation: "" } }
+		assert_not flash.empty?
+		assert_redirected_to @user
+		@user.reload
+		assert_equal name, @user.name
+		assert_equal email, @user.email
 	end
 
 end
