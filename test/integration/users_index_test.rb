8f89ea28dfc18070d7f64658319e783cfcf1bc07
Rebecca <rblum@patientslikeme.com>
2017-12-31 15:06:01 -0500

    Complete account activation via email

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 	def setup
 		@admin = users(:rebecca)
 		@non_admin = users(:archer)
+		@non_activated = users(:lana)
 	end
 
 	test "index as admin including pagination and delete links" do
@@ -12,9 +13,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 		get users_path
 		assert_template 'users/index'
 		assert_select 'div.pagination', count: 2
-		first_page_of_users = User.paginate(page: 1)
+		# how to guarantee that paginating here is the same as what's actually on the page?
+		first_page_of_users = User.where(activated: true).paginate(page: 1)
 		first_page_of_users.each do |user|
 			assert_select  'a[href=?]', user_path(user), text: user.name
+			assert user.activated?
 			unless user == @admin
 				assert_select 'a[href=?]', user_path(user), text: 'delete'
 			end
@@ -30,6 +33,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 		assert_select 'a', text: 'delete', count: 0
 	end
 
+	test "redirect from non-activated user page" do
+		log_in_as(@non_admin)
+		get user_path(@non_activated)
+		assert_redirected_to root_url
+	end
 
 
 end
