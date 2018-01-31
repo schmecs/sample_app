67f1c3489eba8381818c558338e6954e91fc76c9
Rebecca <rblum@patientslikeme.com>
2018-01-31 07:15:20 -0500

    remove email from user index serializers

--- app/serializers/api/v1/user_index_serializer.rb
+++ app/serializers/api/v1/user_index_serializer.rb
@@ -1,7 +1,7 @@
 class Api::V1::UserIndexSerializer < ActiveModel::Serializer
-  attributes :name, :email
+  attributes :name
 
- #  has_many :microposts, serializer: Api::V1::MicropostSerializer
+ 	# has_many :microposts, serializer: Api::V1::MicropostSerializer
 	# has_many :following, through: :active_relationships, source: :followed, serializer: Api::V1::UserSerializer
 	# has_many :followers, through: :passive_relationships, source: :follower, serializer: Api::V1::UserSerializer
   
